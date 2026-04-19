9cc5eff9de39a1d521f2dafc17dec45fb1987aa2
dasonrblx <jayjustion@gmail.com>
2026-04-19 01:43:35 -0400

    Update graphs.py

--- PythonProject1/charts/graphs.py
+++ PythonProject1/charts/graphs.py
@@ -138,8 +138,8 @@ def make_comparison_chart(
     yaxis_config = dict(**_LAYOUT["yaxis"])
     if y_range is not None:
         yaxis_config["range"] = y_range
-        
-        fig.update_layout(
+
+    fig.update_layout(
         **_LAYOUT,
         title=dict(text="Multi-Stock Comparison", font=dict(size=16)),
         xaxis_title="Time",
@@ -178,6 +178,7 @@ def make_comparison_chart(
     
     fig.update_yaxes(yaxis_config)
     
+    return fig
     return fig
      rangeslider=dict(visible=True, bgcolor="#0d1117", thickness=0.06),
         type="date",
